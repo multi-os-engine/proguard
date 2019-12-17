a07b873d351ce51d883d416c4fe8cc4e5cb3e929
noisyfox <timemanager.rick@gmail.com>
2019-12-18 00:26:19 +1100

    Simplify outer class find

--- core/src/proguard/shrink/MemberClassUsageMarker.java
+++ core/src/proguard/shrink/MemberClassUsageMarker.java
@@ -1,8 +1,8 @@
 package proguard.shrink;
 
 import proguard.classfile.*;
-import proguard.classfile.attribute.*;
-import proguard.classfile.attribute.visitor.InnerClassesInfoVisitor;
+import proguard.classfile.attribute.InnerClassesInfo;
+import proguard.classfile.attribute.visitor.*;
 import proguard.classfile.constant.ClassConstant;
 import proguard.classfile.constant.visitor.DummyConstantVisitor;
 import proguard.classfile.visitor.ClassVisitor;
@@ -17,7 +17,8 @@ public class MemberClassUsageMarker extends UsageMarker {
         return classVisitor;
     }
 
-    private static class OuterClassFinder implements InnerClassesInfoVisitor {
+    private static class OuterClassFinder
+    implements InnerClassesInfoVisitor {
 
         public ProgramClass outerClass = null;
         private ProgramClass innerClass = null;
@@ -49,29 +50,25 @@ public class MemberClassUsageMarker extends UsageMarker {
         if (shouldBeMarkedAsUsed(programClass)) {
             boolean isOuterClassUsed = false;
 
-            for (int index = 0; index < programClass.u2attributesCount; index++)
+            // Find the outer class
+            OuterClassFinder outerClassFinder = new OuterClassFinder();
+            programClass.accept(new AllAttributeVisitor(
+                                new AllInnerClassesInfoVisitor(
+                                outerClassFinder)));
+
+            // Check if outer class is used
+            if (outerClassFinder.outerClass != null)
             {
-                Attribute attribute = programClass.attributes[index];
-                if (attribute.getAttributeName(programClass).equals(ClassConstants.ATTR_InnerClasses))
-                {
-                    // Find the outer class
-                    OuterClassFinder outerClassFinder = new OuterClassFinder();
-                    ((InnerClassesAttribute) attribute).innerClassEntriesAccept(programClass, outerClassFinder);
-
-                    if (outerClassFinder.outerClass != null)
-                    {
-                        isOuterClassUsed = isUsed(outerClassFinder.outerClass);
-                    }
-
-                    break;
-                }
+                isOuterClassUsed = isUsed(outerClassFinder.outerClass);
             }
 
-            if (isOuterClassUsed) {
+            if (isOuterClassUsed)
+            {
                 markAsUsed(programClass);
                 markProgramClassBody(programClass);
-
-            } else if (shouldBeMarkedAsPossiblyUsed(programClass)) {
+            }
+            else if (shouldBeMarkedAsPossiblyUsed(programClass))
+            {
                 markAsPossiblyUsed(programClass);
             }
         }
